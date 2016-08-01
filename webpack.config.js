884d232f121879e629567e30313bef1895826aca
JosephPena <arcx354@gmail.com>
2016-08-01 15:55:11 -0700

    first draft

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+//gameplan is to create a new plugin
+module.exports = {
+    entry: "./app/index.js",
+    output: {
+        path: __dirname, + '/dist',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    }
+};
